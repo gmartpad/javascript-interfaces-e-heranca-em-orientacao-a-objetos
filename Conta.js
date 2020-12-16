fdb228123fe53c93bf0b374a2ffe11f2c81f592a
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-16 00:37:06 -0300

    Compartilhando código

---
+++ Conta.js
@@ -0,0 +1,36 @@
+export class Conta {
+    
+    constructor(saldoInicial, cliente, agencia){
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    //------------------------------------------------------------
+
+    sacar(valorSacado){
+        if(this._saldo < valorSacado){
+            return;
+        }
+        this._saldo -= valorSacado;
+        return valorSacado;
+    }
+
+    depositar(valorDepositado){
+        if(valorDepositado <= 0){
+            return;
+        }
+        this._saldo += valorDepositado;
+        return valorDepositado;
+    }
+
+    transferir(valorTransferido, contaBeneficiaria){
+        const valorSacado = this.sacar(valorTransferido);
+        if(isNaN(valorSacado)){
+            console.error("Não foi possível realizar transferência, saldos insuficientes")
+            return;
+        }
+        contaBeneficiaria.depositar(valorSacado);
+    }
+
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Jose", 11122233309);
 
-const contaCorrenteJose = new ContaCorrente(cliente1, 1001);
+const contaCorrenteJose = new Conta(0, cliente1, 1001);
 
 // console.log(contaCorrenteJoana);
 
@@ -16,7 +17,7 @@ console.log(contaCorrenteJose);
 
 // contaCorrenteJose.transferir(20, contaCorrenteJoana)
 
-const contaPoupancaJose = new ContaPoupanca(contaCorrenteJose.sacar(30), cliente1, contaCorrenteJose.agencia)
+const contaPoupancaJose = new Conta(contaCorrenteJose.sacar(30), cliente1, contaCorrenteJose.agencia)
 
 console.log(contaPoupancaJose);
 console.log(contaCorrenteJose);
