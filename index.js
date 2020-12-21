3a92be889f8e2f06b1e38ec1388b324f46b2b290
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-21 20:18:27 -0300

    Criando funcionarios

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { Cliente } from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta/Conta.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from './Conta/ContaSalario.js';
 
 const cliente1 = new Cliente("Jose", 11122233309);
 
